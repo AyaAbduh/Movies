1dc08f44f8683f71bb6c511f59a550644484920a
Aya Abdo <aya.yoya962015@gmail.com>
2025-04-13 16:54:18 +0200

    handling navigation component with fav screen

--- app/src/main/java/com/example/movies/presentation/NavGraphActivity.kt
+++ app/src/main/java/com/example/movies/presentation/NavGraphActivity.kt
@@ -2,7 +2,11 @@ package com.example.movies.presentation
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.movies.R
 import com.example.movies.data.NetworkUtil
@@ -12,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class NavGraphActivity : AppCompatActivity() {
 
 
+    lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_graph)
@@ -19,7 +24,7 @@ class NavGraphActivity : AppCompatActivity() {
         NetworkUtil.startNetworkMonitoring(this)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+         navController = navHostFragment.navController
 
     }
 
@@ -27,4 +32,18 @@ class NavGraphActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main,menu)
         return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_favorite -> {
+                if(navController.currentDestination?.id==R.id.moviesListFragment)
+                    navController.navigate(R.id.action_moviesListFragment_to_favouriteFragment)
+                else if(navController.currentDestination?.id==R.id.movieDetailsFragment)
+                    navController.navigate(R.id.action_movieDetailsFragment_to_favouriteFragment2)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
